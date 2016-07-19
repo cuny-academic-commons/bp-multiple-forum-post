0bc199c79f614a4726556d2f9c3dd93b60399c5f
r-a-y <r-a-y@gmx.com>
2016-07-18 23:10:55 -0700

    Fix issue with empty post content in GES when no extra groups are selected.

--- includes/email-notifications.php
+++ includes/email-notifications.php
@@ -15,7 +15,7 @@ function bpmfp_add_duplicate_topics_to_email_notification( $content, $activity )
 	$topic_id = $activity->secondary_item_id;
 	$all_topic_ids = bpmfp_get_duplicate_topics_ids( $topic_id );
 	if ( empty( $all_topic_ids ) ) {
-		return;
+		return $content;
 	}
 	$duplicate_post_message = "\n" . bpmfp_get_this_topic_also_posted_in_message( $all_topic_ids, 'email' );
 
