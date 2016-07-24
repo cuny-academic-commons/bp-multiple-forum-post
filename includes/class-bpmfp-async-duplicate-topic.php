7199ba5053bf0cde05b6c03aac4dff0585ca1e7d
r-a-y <r-a-y@gmx.com>
2016-07-24 12:37:31 -0700

    Whitespace formatting.

--- includes/class-bpmfp-async-duplicate-topic.php
+++ includes/class-bpmfp-async-duplicate-topic.php
@@ -34,13 +34,14 @@ class BPMFP_Async_Duplicate_Topic extends WP_Async_Task {
 		}
 
 		$topic_id = $data[0];
+
 		return array(
-					'topic-id' => $topic_id,
-					'groups-to-post-to' => $groups_to_post_to,
-					'topic-tags' => $_POST['bbp_topic_tags'],
-					'topic-content' => $_POST['bbp_topic_content'],
-					'topic-title' => $_POST['bbp_topic_title'],
-				);
+			'topic-id'          => $topic_id,
+			'groups-to-post-to' => $_POST['groups-to-post-to'],
+			'topic-tags'        => $_POST['bbp_topic_tags'],
+			'topic-content'     => $_POST['bbp_topic_content'],
+			'topic-title'       => $_POST['bbp_topic_title'],
+		);
 	}
 
 	/**
@@ -54,10 +55,10 @@ class BPMFP_Async_Duplicate_Topic extends WP_Async_Task {
 	protected function run_action() {
 		$args = array();
 
-		$args['topic-id'] = $_POST['topic-id'];
-		$args['topic-tags'] = $_POST['topic-tags'];
-		$args['topic-content'] = $_POST['topic-content'];
-		$args['topic-title'] = $_POST['topic-title'];
+		$args['topic-id']          = $_POST['topic-id'];
+		$args['topic-tags']        = $_POST['topic-tags'];
+		$args['topic-content']     = $_POST['topic-content'];
+		$args['topic-title']       = $_POST['topic-title'];
 		$args['groups-to-post-to'] = $_POST['groups-to-post-to'];
 
 		do_action( 'wp_async_' . $this->action, $args );
